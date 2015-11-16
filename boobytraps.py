c4a82150a20165e18c10b3505216be6b8d555d04
Noah Doersing <doersino@gmail.com>
2015-11-16 13:28:55 +0100

    Restructure solution

--- boobytraps.py
+++ boobytraps.py
@@ -1,43 +1,51 @@
-# TODO create type for positions, map?
 # TODO rethink function names, class needed?
 
 import fileinput
 
 
-class BoobyTraps:
-    def __init__(self, input):
-        self.dominationOrder = input[0]
+class Coords:
+    x = 0
+    y = 0
 
-        #self.mapWidth = int(input[1][0])
-        self.mapHeight = int(input[1][2])
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-        self.map = input[2:self.mapHeight+2]
+    def __str__(self):
+        return 'x: ' + str(self.x) + ', y: ' + str(self.y)
 
-        self.startX = int(input[self.mapHeight+2][0])
-        self.startY = int(input[self.mapHeight+2][2])
 
-        self.endX = int(input[self.mapHeight+3][0])
-        self.endY = int(input[self.mapHeight+3][2])
+class Map:
+    map = None
+    trapDominationOrder = None
 
-    #TODO return map with all smaller traps set to o
-    def updateMap(self, map, trapTriggered):
+    # given as array (rows) of array (fields)
+    def __init__(self, map, trapDominationOrder):
+        self.map = map
+        self.trapDominationOrder = trapDominationOrder
+
+    def __str__(self):
+        return 'map: ' + str(self.map) + ', trapDominationOrder: ' + self.trapDominationOrder
+
+    #TODO copy constructor?
+
+    #TODO set triggered (<(=?) trapTriggered)) to o
+    def updateTraps(self, trapTriggered):
         pass
 
-    #TODO get adjacient fields that aren't walls
-    def getAdjacient(self, map, x, y):
-        pass
+    def getAdjacient(self, coords):
+        pass  # TODO
 
-    #TODO implement
-    def getMinMoves(self, map, start, end):
-        #for adj in self.getAdjacient(map, start[0], start[1]):
-        #    pass
-        return 17
+    def getAt(self, coords):
+        return self.map[coords.y][coords.x]
 
-    def raidtomb(self):
-        map = self.map
-        start = [self.startX, self.startY]
-        end = [self.endX, self.endY]
-        return self.getMinMoves(map, start, end)
+    def setAt(self, coords, char):
+        self.map[coords.y][coords.x] = char
+
+
+# TODO implement
+def raidtomb(map, start, end):
+    return 17
 
 
 def main():
@@ -46,13 +54,25 @@ def main():
     for line in fileinput.input():
         input.append(line.strip())
 
-    # create BoobyTraps object from input
-    boobytraps = BoobyTraps(input)
+    # parse input
+    trapDominationOrder = input[0]
+    #mapWidth = int(input[1][0])
+    mapHeight = int(input[1][2])
+    map = Map(input[2:mapHeight+2], trapDominationOrder)
+
+    startX = int(input[mapHeight+2][0])
+    startY = int(input[mapHeight+2][2])
+    start = Coords(startX, startY)
+
+    endX = int(input[mapHeight+3][0])
+    endY = int(input[mapHeight+3][2])
+    end = Coords(endX, endY)
 
     # compute and output minimum number of moves needed to reach the end
     # position from the start position
-    minMoves = boobytraps.raidtomb()
-    print minMoves
+    print raidtomb(map, start, end)
+
+    #print map
 
 if __name__ == "__main__":
     main()
