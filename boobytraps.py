69235262b140d979b8ecc526198b555a6e6127fa
Noah Doersing <doersino@gmail.com>
2015-11-16 21:08:45 +0100

    Added buggy tomb raider function

--- boobytraps.py
+++ boobytraps.py
@@ -1,8 +1,12 @@
 #TODO maybe cell class with coords and value, change map accordingly
 #TODO getAt, setAt neccessary, especially in the map class itself?
+#TODO """-style function comments
+#TODO maybe identify coords by integer, like width*y+x
+#TODO print IMPOSSIBLE if impossible
 
 import fileinput
 import copy
+import sys
 
 
 class Coords:
@@ -12,6 +16,12 @@ class Coords:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash(self.x * self.y)  # TODO maybe not a good hash?
 
     def __str__(self):
         return 'x: ' + str(self.x) + ', y: ' + str(self.y)
@@ -33,7 +43,7 @@ class Map:
 
         self.trapDominationOrder = list(trapDominationOrder)
 
-    def __str__(self):  # TODO make prettier
+    def __str__(self):  # TODO make prettier, with coord "axes"?, maybe color
         return 'map: ' + str(self.map) + ', trapDominationOrder: ' + str(self.trapDominationOrder)
 
     # returns a copy of this map
@@ -55,8 +65,8 @@ class Map:
                 if field in triggeredTraps:
                     self.setAt(Coords(x, y), 'x')
 
-    # return array of (max four) adjacient coords that aren't x
-    def getAdjacient(self, coords):
+    # return array of (max four) adjacent coords that aren't x
+    def getAdjacent(self, coords):
         adj = []
 
         # left
@@ -85,16 +95,50 @@ class Map:
 
         return adj
 
+    def getVisitables(self):
+        visitable = []
+        for y, row in enumerate(self.map):
+            for x, field in enumerate(row):
+                if field in ['o'] + self.trapDominationOrder:  # this works because any triggered traps will have been converted to x
+                    visitable.append(Coords(x, y))
+
+        return visitable
+
+
     def getAt(self, coords):
         return self.map[coords.y][coords.x]
 
     def setAt(self, coords, char):
         self.map[coords.y][coords.x] = char
 
+    def isTrap(self, char):
+        return char in self.trapDominationOrder
+
 
 # TODO implement, possibly based on http://rebrained.com/?p=392
-def raidtomb(map, start, end):
-    return 17
+def raidtomb(map, start, end, visited=[], distances={}, predecessors={}):
+    print start
+    if not visited:
+        distances[start] = 0
+    if start == end:
+        path = []
+        while end != None:
+            path.append(end)
+            end = predecessors.get(end)
+        return distances[start], path[::-1]
+    for neighbor in map.getAdjacent(start):
+        if neighbor not in visited:
+            neighbordist = distances.get(neighbor, sys.maxint)
+            tentativedist = distances[start] + 1
+            if tentativedist < neighbordist:
+                distances[neighbor] = tentativedist
+                predecessors[neighbor] = start
+    visited.append(start)
+    if map.isTrap(map.getAt(start)):
+        map.updateTraps(map.getAt(start))
+    unvisiteds = dict((k, distances.get(k,sys.maxint)) for k in map.getVisitables() if k not in visited)
+    closestnode = min(unvisiteds, key=unvisiteds.get)
+    return raidtomb(map,closestnode,end,visited,distances,predecessors)
 
 
 def main():
@@ -122,14 +166,16 @@ def main():
     print raidtomb(map, start, end)
     #print map
 
-    # test getAdjacient
+    # test getAdjacent
     #print map.getAt(Coords(3, 5))
-    #for i in map.getAdjacient(Coords(3, 5)):
+    #for i in map.getAdjacent(Coords(3, 5)):
     #    print i
     #    print map.getAt(i)
     #print map.getAt(Coords(1, 2))
 
-    #map.updateTraps('B')
+    #print map
+    #if map.isTrap('o'):
+    #    map.updateTraps('o')
     #print map
 
 if __name__ == "__main__":
