4ae68cf848dc59e26f12ed39ca00f01c2d081cfd
Noah Doersing <doersino@gmail.com>
2015-11-22 12:39:48 +0100

    Add option to specify seed to map generator

--- gravedigger.py
+++ gravedigger.py
@@ -5,15 +5,25 @@
 
 import argparse
 import sys
+import random
 
 # parse options
 parser = argparse.ArgumentParser()
 parser.add_argument("width", metavar="WIDTH", type=int, choices=xrange(1, 40000), help="desired width of the map")
 parser.add_argument("height", metavar="HEIGHT", type=int, choices=xrange(1, 40000), help="desired height of the map")
+parser.add_argument("--seed", help="seed for map generation")
 args = parser.parse_args()
 
 if args.width * args.height > 40000:
     sys.exit("usage: " + __file__ + " [-h] WIDTH HEIGHT\n" + __file__ + ": error: WIDTH * HEIGHT must not exceed 40000")
+
+if not args.seed:
+    rand = random.SystemRandom()
+    args.seed = rand.random()
+
+# initialize random number generator
+seed = args.seed
+random.seed(seed)
 
 # generate map
 trapDominationOrdering = "ZYXWVUTSRQPONMLKJIHGFEDCBA"
@@ -22,6 +32,7 @@ height = args.height
 map = [['x' for field in range(width)] for row in range(height)]
 
 #TODO fill map
+
 
 # set start and end points, make sure they aren't walls
 #TODO randomize
