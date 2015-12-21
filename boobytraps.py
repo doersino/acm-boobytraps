26b11473de5704d2db6e37bf2b35c1d5f484b2c3
Noah Doersing <doersino@gmail.com>
2015-12-21 17:08:50 +0100

    Remove unneeded code

--- boobytraps.py
+++ boobytraps.py
@@ -150,10 +150,6 @@ class Map:
         """Get the cell value at position x, y."""
         return self.map[y][x]
 
-    def setAt(self, cell):
-        """Set the cell value."""
-        self.map[cell.y][cell.x] = cell.value
-
 
 class Graph:
     """Graph represented as an adjacency list."""
@@ -176,8 +172,6 @@ class Graph:
                         adjDict[i] = 1
                     self.graph[cell] = adjDict
 
-        self.optimize()
-
     def __str__(self):
         return 'map: ' + str(self.map) + ', graph: ' + str(self.graph)
 
@@ -190,51 +184,8 @@ class Graph:
             sys.stdout.write("}")
             print
 
-    def optimize(self):
-        """Reduce size and complexity of graph by removing deadends and collapsing adjacent cells with no branches to other cells."""
-        # do all this recursively until no change?
-        # if two neigbours and in line with no other neighbors, concat
-        # if only one neighbor and both not traps, concat
-        #for field in self.graph:
-        #    if len(self.graph[field]) == 1:
-        #        self.graph[field] = self.graph[field][self.graph[field].keys()[0]]
-        #        del self.graph[self.graph[field][self.graph[field].keys()[0]]]
-        # remove deadends (if != start, end)
-        # ---
-        #for field in list(self.graph):
-        #    try:
-        #        if len(self.graph[field]) == 1 and field.value == 'o':
-        #            for i in self.graph[field]:
-        #                child = i
-        #            if child.value == 'o':
-        #                print field
-        #                print self.graph[field].keys()[0]
-        #                self.graph[field] = self.graph[child]
-        #                del self.graph[child]
-        #    except KeyError:
-        #        pass
-        pass
 
-    def update(self, triggeredTrapCell):
-        # compute list of triggered traps
-        triggeredTraps = []
-        for trap in self.map.traps.trapDominationOrder:
-            triggeredTraps.append(trap)
-            if trap == triggeredTrapCell.value:
-                break
-
-        # remove all referecences to triggered traps from the graph
-        for cell in list(self.graph):
-            if cell.value in triggeredTraps:
-                del self.graph[cell]
-                for cell2 in self.graph:
-                    if cell in self.graph[cell2]:
-                        del self.graph[cell2][cell]
-
-        self.optimize()
-
-
-def raidtombBacktracking(graph, start, end):
+def raidtomb(graph, start, end):
     """Find the shortest path between start and end cells ("raid the tomb") using backtracking"""
 
     sstart = start
@@ -288,59 +239,6 @@ def raidtombBacktracking(graph, start, end):
     return "IMPOSSIBLE"
 
 
-def raidtomb(graph, start, end, visited=[], distances={}, predecessors={}):
-    """Find the shortest path between start and end cells ("raid the tomb")"""
-    # algorithm based on http://rebrained.com/?p=392
-
-    # detect if it's the first time through, set current distance to zero
-    if not visited:
-        distances[start] = 0
-
-    if start == end:
-        # we've found our end node, now find the path to it, and return
-        path = []
-        while end is not None:
-            path.append(end)
-            end = predecessors.get(end)
-        return distances[start], path[::-1]
-
-    # process neighbors as per algorithm, keep track of predecessors
-    for neighbor in graph.graph[start]:
-        if neighbor not in visited:
-            neighbordist = distances.get(neighbor, sys.maxint)
-            tentativedist = distances[start] + graph.graph[start][neighbor]
-            if tentativedist < neighbordist:
-                distances[neighbor] = tentativedist
-                predecessors[neighbor] = start
-
-    # neighbors processed, now mark the current node as visited
-    visited.append(start)
-
-    # trigger traps
-    if start.value != 'o':
-        graph = copy.deepcopy(graph)
-        graph.update(start)
-
-    # find the closest reachable unvisited node to the start
-    unvisiteds = dict((k, distances.get(k, sys.maxint)) for k in graph.graph if k not in visited and distances.get(k, sys.maxint) < sys.maxint)
-    if not unvisiteds:
-        return "IMPOSSIBLE"
-    closestnode = min(unvisiteds, key=unvisiteds.get)
-
-    while unvisiteds.pop(closestnode, False):
-        # now we can take the closest node and recurse, making it current
-        raided = raidtomb(graph, closestnode, end, copy.deepcopy(visited), copy.deepcopy(distances), predecessors)
-
-        # return to recursive call before most recent trap cell (i.e. recursive call with trap cell as closestnode)
-        if raided == "IMPOSSIBLE" and closestnode.value != 'o' and unvisiteds:
-            # ignore the trap cell and try other adjacent cells
-            closestnode = min(unvisiteds, key=unvisiteds.get)
-        else:
-            # only if that fails and if no previous recursive trap cell call was made, we can be sure that the previous return value is correct
-            return raided
-    return "IMPOSSIBLE"
-
-
 def main():
     # process verbose option
     verbose = len(sys.argv) > 1 and sys.argv[1] == "-v"
@@ -369,8 +267,7 @@ def main():
 
     # compute and output minimum number of moves needed to reach the end
     # position from the start position ("raid the tomb")
-    #raided = raidtomb(graph, start, end)
-    raided = raidtombBacktracking(graph, start, end)
+    raided = raidtomb(graph, start, end)
 
     # print result
     if verbose:
